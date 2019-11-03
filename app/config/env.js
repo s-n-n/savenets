7c4837cf65cc60a8a3e094766a604882911cc164
nikolay.savenets <nikolay.savenets@exponential.com>
2019-11-04 00:55:43 +0200

    8

--- app/config/env.js
+++ app/config/env.js
@@ -1,6 +1,6 @@
 const env = {
     database: 'heroku_deb8f2418aef4e6',
-    username: 'b376c1a030f581',
+    username: 'b5d9f76a019420',
     password: 'b4a714dd',
     host: 'savenets.herokuapp.com',
     dialect: 'mysql',
