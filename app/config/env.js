2ee069c91c0b4278a4928174887b8e998f8884be
nikolay.savenets <nikolay.savenets@exponential.com>
2019-11-04 15:12:42 +0200

    11

--- app/config/env.js
+++ app/config/env.js
@@ -2,7 +2,7 @@ const env = {
     database: 'heroku_deb8f2418aef4e6',
     username: 'b5d9f76a019420',
     password: 'b4a714dd',
-    host: 'mysql://b5d9f76a019420:b4a714dd@eu-cdbr-west-02.cleardb.net/heroku_deb8f2418aef4e6?reconnect=true',
+    host: 'eu-cdbr-west-02.cleardb.net',
     dialect: 'mysql',
     pool: {
         max: 5,
